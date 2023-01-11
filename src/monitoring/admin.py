2cc53823f6773374105404bd50b431a92760026b
abdourakhman <abdourakhman920@gmail.com>
2023-01-11 03:43:33 +0100

    modele Salle Lit Service work

--- src/monitoring/admin.py
+++ src/monitoring/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from monitoring.models import Patient,Secretaire,Docteur,Salle,Service,Lit,Ordonnance,DossierMedical,RendezVous
 # Register your models here.
+
+personnelMedical=[Patient,Secretaire,Docteur,Salle,Service,Lit,Ordonnance,DossierMedical,RendezVous]
+
+class AdminLit(admin.ModelAdmin):
+    list_display = ('numero','salle','statut')
+
+class AdminSalle(admin.ModelAdmin):
+    list_display=('nom','statut')
+
+admin.site.register(Lit,AdminLit)
+admin.site.register(Salle,AdminSalle)
--- src/monitoring/models.py
+++ src/monitoring/models.py
@@ -6,8 +6,8 @@ class Patient(models.Model):
     age = models.PositiveIntegerField(null=True)
     sexe = models.CharField(max_length=1, choices=[('M', 'Masculin'), ('F', 'Féminin')], null=True)
     groupe_sanguin = models.CharField(max_length=10, null=True)
-    profile = models.ImageField(upload_to='profile_pic/PatientProfilePic/', null=True, blank=True)
     telephone = models.CharField(max_length=20, null=False)
+    profile = models.ImageField(upload_to='profile_pic/PatientProfilePic/', null=True, blank=True)
     secretaire = models.ForeignKey("Secretaire", on_delete=models.SET_NULL, null=True, related_name='patients')
     rendez_vous = models.ManyToManyField("RendezVous", related_name='patients_rendez_vous')
     dossier = models.OneToOneField("DossierMedical", on_delete=models.CASCADE, null=True, related_name='dossier_patient')
@@ -18,8 +18,8 @@ class Docteur(models.Model):
     sexe = models.CharField(max_length=1, choices=[('M', 'Masculin'), ('F', 'Féminin')], null=True)
     telephone = models.CharField(max_length=20, null=False)
     adresse = models.CharField(max_length=40)
-    profile = models.ImageField(upload_to='profile_pic/DocteurProfilePic/', null=True, blank=True)
     specialite = models.CharField(max_length=50)
+    profile = models.ImageField(upload_to='profile_pic/DocteurProfilePic/', null=True, blank=True)
     service = models.ForeignKey("Service", on_delete=models.CASCADE)
     rendez_vous = models.ManyToManyField("RendezVous", related_name='docteurs_rendez_vous')
     ordonnances = models.ManyToManyField("Ordonnance", related_name='docteurs_ordonnances')
@@ -33,23 +33,31 @@ class Secretaire(models.Model):
     adresse = models.CharField(max_length=40)
     profile = models.ImageField(upload_to='profile_pic/SecretaireProfilePic/', null=True, blank=True)
 
-class Service(models.Model):
-    nom = models.CharField(max_length=50, null=False)
-    salles = models.ManyToManyField("Salle", related_name='salles_services')
-    patients = models.ManyToManyField(Patient, related_name='patients_services')
 
 class Salle(models.Model):
-    numero = models.PositiveIntegerField(unique=True,null=False)
-    lits = models.ManyToManyField("Lit", related_name='lits_salles')
-    statut = models.BooleanField(default=False)
+    nom = models.CharField(max_length=10, unique=True)
+    statut = models.BooleanField(default=True)
+    def __str__(self):
+        return f"Salle {self.nom}"
+
+class Service(models.Model):
+    nom = models.CharField(max_length=50, unique=True,null=False)
+    salles = models.ForeignKey(Salle, on_delete=models.CASCADE, default=1)
+    patients = models.ManyToManyField(Patient, related_name='patients_services')
+    def __str__(self):
+        return f"Service_{self.nom}"
 
 class Lit(models.Model):
     numero = models.PositiveIntegerField(unique=True,null=False)
-    statut = models.BooleanField(default=False)
+    salle = models.ForeignKey(Salle, on_delete=models.CASCADE, related_name='lits')
+    statut = models.BooleanField(null=False,default=True)
+    def str(self):
+        return f"lit n°_{self.numero}"
 
 class Ordonnance(models.Model):
     contenu = models.TextField(null=True)
     docteurs = models.ManyToManyField(Docteur, related_name='docteurs_ordonnances')
+    date = models.DateField(auto_now=True)
     dossier_medical = models.ForeignKey("DossierMedical", on_delete=models.CASCADE, related_name='ordonnances')
 
 class DossierMedical(models.Model):
@@ -63,3 +71,7 @@ class RendezVous(models.Model):
     date = models.DateField(null=True)
     docteurs = models.ManyToManyField(Docteur, related_name='docteurs_rendez_vous')
     patients = models.ManyToManyField(Patient, related_name='patients_rendez_vous')
+
+
+
+
